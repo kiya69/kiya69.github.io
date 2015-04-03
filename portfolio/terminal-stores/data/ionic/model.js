bb5fc053eff594c95627539c63ca27125a41b098
kiya69 <kiya69@gmail.com>
2015-04-03 00:55:07 -0400

    added portfolio data

---
+++ portfolio/terminal-stores/data/ionic/model.js
@@ -0,0 +1,540 @@
+{
+  "materials": {
+    "standard_glass": {
+      "Kd": [149, 149, 149]
+    },
+    "exterior_wall_4_003": {
+      "map_Kd": "EXTERIOR-WALL-4-003.jpg"
+    },
+    "exterior_wall_3_003": {
+      "map_Kd": "EXTERIOR-WALL-3-003.jpg"
+    },
+    "exterior_wall_2_003": {
+      "map_Kd": "EXTERIOR-WALL-2-003.jpg"
+    },
+    "exterior_wall_1_003": {
+      "map_Kd": "EXTERIOR-WALL-1-003.jpg"
+    },
+    "exterior_e_entrance_003": {
+      "map_Kd": "EXTERIOR-E-ENTRANCE-003.jpg"
+    },
+    "exterior_w_entrance_003": {
+      "map_Kd": "EXTERIOR-W-ENTRANCE-003.jpg"
+    },
+    "inner_wall_4_003": {
+      "map_Kd": "INNER-WALL-4-003.jpg"
+    },
+    "inner_wall_3_003": {
+      "map_Kd": "INNER-WALL-3-003.jpg"
+    },
+    "inner_wall_2_003": {
+      "map_Kd": "INNER-WALL-2-003.jpg"
+    },
+    "inner_wall_1_003": {
+      "map_Kd": "INNER-WALL-1-003.jpg"
+    },
+    "fire_escape_1_003": {
+      "map_Kd": "FIRE-ESCAPE-1-003.jpg"
+    },
+    "fire_escape_2_003": {
+      "map_Kd": "FIRE-ESCAPE-2-003.jpg"
+    },
+    "center_revit_wall_2_003": {
+      "map_Kd": "CENTER-REVIT-WALL-2-003.jpg"
+    },
+    "entrance_wood_decor_003": {
+      "map_Kd": "ENTRANCE-WOOD-DECOR-003.jpg"
+    },
+    "walkway_railing_003": {
+      "map_Kd": "WALKWAY-RAILING-003.jpg"
+    },
+    "building_letters_003": {
+      "map_Kd": "BUILDING-LETTERS-003.jpg"
+    },
+    "facade_003": {
+      "map_Kd": "FACADE-003.jpg"
+    },
+    "floor_cap_003": {
+      "map_Kd": "FLOOR-CAP-003.jpg"
+    },
+    "new_roof_003": {
+      "map_Kd": "NEW-ROOF-003.jpg"
+    },
+    "center_revit_wall_1_003": {
+      "map_Kd": "CENTER-REVIT-WALL-1-003.jpg"
+    },
+    "exterior_wall_5_003": {
+      "map_Kd": "EXTERIOR-WALL-5-003.jpg"
+    },
+    "elevator_shafts_003": {
+      "map_Kd": "ELEVATOR-SHAFTS-003.jpg"
+    },
+    "floor_inst_4_003": {
+      "map_Kd": "FLOOR-INST-4-003.jpg"
+    },
+    "floor_inst_3_003": {
+      "map_Kd": "FLOOR-INST-3-003.jpg"
+    },
+    "floor_inst_2_003": {
+      "map_Kd": "FLOOR-INST-2-003.jpg"
+    },
+    "floor_inst_1_003": {
+      "map_Kd": "FLOOR-INST-1-003.jpg"
+    },
+    "floor_inst_5_003": {
+      "map_Kd": "FLOOR-INST-5-003.jpg"
+    },
+    "floor_1_5_003": {
+      "map_Kd": "FLOOR-1-5-003.jpg"
+    },
+    "floor_1_4_003": {
+      "map_Kd": "FLOOR-1-4-003.jpg"
+    },
+    "floor_1_3_003": {
+      "map_Kd": "FLOOR-1-3-003.jpg"
+    },
+    "floor_1_2_003": {
+      "map_Kd": "FLOOR-1-2-003.jpg"
+    },
+    "floor_1_1_003": {
+      "map_Kd": "FLOOR-1-1-003.jpg"
+    },
+    "floor_1_6_003": {
+      "map_Kd": "FLOOR-1-6-003.jpg"
+    },
+    "exterior_windows_4_003": {
+      "map_Kd": "EXTERIOR-WINDOWS-4-003.jpg"
+    },
+    "exterior_windows_3_003": {
+      "map_Kd": "EXTERIOR-WINDOWS-3-003.jpg"
+    },
+    "exterior_windows_2_003": {
+      "map_Kd": "EXTERIOR-WINDOWS-2-003.jpg"
+    },
+    "exterior_windows_1_003": {
+      "map_Kd": "EXTERIOR-WINDOWS-1-003.jpg"
+    },
+    "ground_level_frames_003": {
+      "map_Kd": "GROUND-LEVEL-FRAMES-003.jpg"
+    },
+    "entrance_mullions_003": {
+      "map_Kd": "ENTRANCE-MULLIONS-003.jpg"
+    },
+    "exterior_windows_5_003": {
+      "map_Kd": "EXTERIOR-WINDOWS-5-003.jpg"
+    },
+    "side_walk_003": {
+      "map_Kd": "Side-Walk-003.jpg"
+    },
+    "street_names": {
+      "map_Kd": "Street-Names.png"
+    }
+  },
+  "decodeParams": {
+    "decodeOffsets": [-7355, -3570, -392, 0, 0, -511, -511, -511],
+    "decodeScales": [0.016309, 0.016309, 0.016309, 0.000978, 0.000978, 0.001957, 0.001957, 0.001957]
+  },
+  "urls": {
+    "f47bbb7c.model.utf8": [{
+      "material": "building_letters_003",
+      "attribRange": [0, 17128],
+      "indexRange": [137024, 11124],
+      "bboxes": 170396,
+      "names": ["building_letters_003"],
+      "lengths": [33372]
+    }],
+    "396d2c14.model.utf8": [{
+      "material": "center_revit_wall_1_003",
+      "attribRange": [0, 20757],
+      "indexRange": [166056, 17384],
+      "bboxes": 218208,
+      "names": ["center_revit_wall_1_003"],
+      "lengths": [52152]
+    }],
+    "a6d407a0.model.utf8": [{
+      "material": "center_revit_wall_2_003",
+      "attribRange": [0, 1071],
+      "indexRange": [8568, 766],
+      "bboxes": 10866,
+      "names": ["center_revit_wall_2_003"],
+      "lengths": [2298]
+    }],
+    "3ae90d4a.model.utf8": [{
+      "material": "elevator_shafts_003",
+      "attribRange": [0, 6490],
+      "indexRange": [51920, 6376],
+      "bboxes": 71048,
+      "names": ["elevator_shafts_003"],
+      "lengths": [19128]
+    }],
+    "544f8de0.model.utf8": [{
+      "material": "entrance_mullions_003",
+      "attribRange": [0, 16328],
+      "indexRange": [130624, 9735],
+      "bboxes": 159829,
+      "names": ["entrance_mullions_003"],
+      "lengths": [29205]
+    }],
+    "642117e4.model.utf8": [{
+      "material": "entrance_wood_decor_003",
+      "attribRange": [0, 2068],
+      "indexRange": [16544, 1036],
+      "bboxes": 19652,
+      "names": ["entrance_wood_decor_003"],
+      "lengths": [3108]
+    }],
+    "5f4de37f.model.utf8": [{
+      "material": "exterior_e_entrance_003",
+      "attribRange": [0, 8962],
+      "indexRange": [71696, 11172],
+      "bboxes": 105212,
+      "names": ["exterior_e_entrance_003"],
+      "lengths": [33516]
+    }],
+    "528a5060.model.utf8": [{
+      "material": "exterior_w_entrance_003",
+      "attribRange": [0, 15624],
+      "indexRange": [124992, 23828],
+      "bboxes": 196476,
+      "names": ["exterior_w_entrance_003"],
+      "lengths": [71484]
+    }],
+    "065c4120.model.utf8": [{
+      "material": "exterior_wall_1_003",
+      "attribRange": [0, 18808],
+      "indexRange": [150464, 23789],
+      "bboxes": 221831,
+      "names": ["exterior_wall_1_003"],
+      "lengths": [71367]
+    }],
+    "f08661d2.model.utf8": [{
+      "material": "exterior_wall_2_003",
+      "attribRange": [0, 12448],
+      "indexRange": [99584, 15273],
+      "bboxes": 145403,
+      "names": ["exterior_wall_2_003"],
+      "lengths": [45819]
+    }],
+    "f8fc37b8.model.utf8": [{
+      "material": "exterior_wall_3_003",
+      "attribRange": [0, 22055],
+      "indexRange": [176440, 29196],
+      "bboxes": 264028,
+      "names": ["exterior_wall_3_003"],
+      "lengths": [87588]
+    }],
+    "8f26d78b.model.utf8": [{
+      "material": "exterior_wall_4_003",
+      "attribRange": [0, 17118],
+      "indexRange": [136944, 22544],
+      "bboxes": 204576,
+      "names": ["exterior_wall_4_003"],
+      "lengths": [67632]
+    }],
+    "66fd8b8c.model.utf8": [{
+      "material": "exterior_wall_5_003",
+      "attribRange": [0, 22039],
+      "indexRange": [176312, 28414],
+      "bboxes": 261554,
+      "names": ["exterior_wall_5_003"],
+      "lengths": [85242]
+    }],
+    "8be0eec6.model.utf8": [{
+      "material": "exterior_windows_1_003",
+      "attribRange": [0, 55294],
+      "indexRange": [442352, 32670],
+      "bboxes": 850528,
+      "names": ["exterior_windows_1_003"],
+      "lengths": [98010]
+    }, {
+      "material": "exterior_windows_1_003",
+      "attribRange": [540362, 28732],
+      "indexRange": [770218, 26770],
+      "bboxes": 850534,
+      "names": ["exterior_windows_1_003"],
+      "lengths": [80310]
+    }],
+    "5e28576d.model.utf8": [{
+      "material": "exterior_windows_2_003",
+      "attribRange": [0, 55294],
+      "indexRange": [442352, 36162],
+      "bboxes": 900192,
+      "names": ["exterior_windows_2_003"],
+      "lengths": [108486]
+    }, {
+      "material": "exterior_windows_2_003",
+      "attribRange": [550838, 32426],
+      "indexRange": [810246, 29982],
+      "bboxes": 900198,
+      "names": ["exterior_windows_2_003"],
+      "lengths": [89946]
+    }],
+    "36ecf788.model.utf8": [{
+      "material": "exterior_windows_3_003",
+      "attribRange": [0, 55294],
+      "indexRange": [442352, 32308],
+      "bboxes": 644992,
+      "names": ["exterior_windows_3_003"],
+      "lengths": [96924]
+    }, {
+      "material": "exterior_windows_3_003",
+      "attribRange": [539276, 9772],
+      "indexRange": [617452, 9180],
+      "bboxes": 644998,
+      "names": ["exterior_windows_3_003"],
+      "lengths": [27540]
+    }],
+    "f9a711e4.model.utf8": [{
+      "material": "exterior_windows_4_003",
+      "attribRange": [0, 42572],
+      "indexRange": [340576, 26180],
+      "bboxes": 419116,
+      "names": ["exterior_windows_4_003"],
+      "lengths": [78540]
+    }],
+    "30a6d3a7.model.utf8": [{
+      "material": "exterior_windows_5_003",
+      "attribRange": [0, 55296],
+      "indexRange": [442368, 30100],
+      "bboxes": 882723,
+      "names": ["exterior_windows_5_003"],
+      "lengths": [90300]
+    }, {
+      "material": "exterior_windows_5_003",
+      "attribRange": [532668, 32946],
+      "indexRange": [796236, 28829],
+      "bboxes": 882729,
+      "names": ["exterior_windows_5_003"],
+      "lengths": [86487]
+    }],
+    "85eab327.model.utf8": [{
+      "material": "facade_003",
+      "attribRange": [0, 42826],
+      "indexRange": [342608, 25332],
+      "bboxes": 418604,
+      "names": ["facade_003"],
+      "lengths": [75996]
+    }],
+    "ca67764b.model.utf8": [{
+      "material": "fire_escape_1_003",
+      "attribRange": [0, 55295],
+      "indexRange": [442360, 27647],
+      "bboxes": 1609456,
+      "names": ["fire_escape_1_003"],
+      "lengths": [82941]
+    }, {
+      "material": "fire_escape_1_003",
+      "attribRange": [525301, 55294],
+      "indexRange": [967653, 35078],
+      "bboxes": 1609462,
+      "names": ["fire_escape_1_003"],
+      "lengths": [105234]
+    }, {
+      "material": "fire_escape_1_003",
+      "attribRange": [1072887, 51323],
+      "indexRange": [1483471, 41995],
+      "bboxes": 1609468,
+      "names": ["fire_escape_1_003"],
+      "lengths": [125985]
+    }],
+    "757e2e99.model.utf8": [{
+      "material": "fire_escape_2_003",
+      "attribRange": [0, 55295],
+      "indexRange": [442360, 27647],
+      "bboxes": 1278104,
+      "names": ["fire_escape_2_003"],
+      "lengths": [82941]
+    }, {
+      "material": "fire_escape_2_003",
+      "attribRange": [525301, 55294],
+      "indexRange": [967653, 40414],
+      "bboxes": 1278110,
+      "names": ["fire_escape_2_003"],
+      "lengths": [121242]
+    }, {
+      "material": "fire_escape_2_003",
+      "attribRange": [1088895, 17989],
+      "indexRange": [1232807, 15099],
+      "bboxes": 1278116,
+      "names": ["fire_escape_2_003"],
+      "lengths": [45297]
+    }],
+    "3302a6a2.model.utf8": [{
+      "material": "floor_1_1_003",
+      "attribRange": [0, 227],
+      "indexRange": [1816, 166],
+      "bboxes": 2314,
+      "names": ["floor_1_1_003"],
+      "lengths": [498]
+    }],
+    "1ba0f16e.model.utf8": [{
+      "material": "floor_1_2_003",
+      "attribRange": [0, 32],
+      "indexRange": [256, 22],
+      "bboxes": 322,
+      "names": ["floor_1_2_003"],
+      "lengths": [66]
+    }],
+    "36d96fca.model.utf8": [{
+      "material": "floor_1_3_003",
+      "attribRange": [0, 61],
+      "indexRange": [488, 51],
+      "bboxes": 641,
+      "names": ["floor_1_3_003"],
+      "lengths": [153]
+    }],
+    "45310b19.model.utf8": [{
+      "material": "floor_1_4_003",
+      "attribRange": [0, 86],
+      "indexRange": [688, 74],
+      "bboxes": 910,
+      "names": ["floor_1_4_003"],
+      "lengths": [222]
+    }],
+    "83139c19.model.utf8": [{
+      "material": "floor_1_5_003",
+      "attribRange": [0, 31],
+      "indexRange": [248, 25],
+      "bboxes": 323,
+      "names": ["floor_1_5_003"],
+      "lengths": [75]
+    }],
+    "f38e8c97.model.utf8": [{
+      "material": "floor_1_6_003",
+      "attribRange": [0, 35],
+      "indexRange": [280, 27],
+      "bboxes": 361,
+      "names": ["floor_1_6_003"],
+      "lengths": [81]
+    }],
+    "c0ea3e55.model.utf8": [{
+      "material": "floor_cap_003",
+      "attribRange": [0, 8843],
+      "indexRange": [70744, 4951],
+      "bboxes": 85597,
+      "names": ["floor_cap_003"],
+      "lengths": [14853]
+    }],
+    "7ef4b5c6.model.utf8": [{
+      "material": "floor_inst_1_003",
+      "attribRange": [0, 228],
+      "indexRange": [1824, 240],
+      "bboxes": 2544,
+      "names": ["floor_inst_1_003"],
+      "lengths": [720]
+    }],
+    "3fd498d8.model.utf8": [{
+      "material": "floor_inst_2_003",
+      "attribRange": [0, 384],
+      "indexRange": [3072, 248],
+      "bboxes": 3816,
+      "names": ["floor_inst_2_003"],
+      "lengths": [744]
+    }],
+    "358dcedd.model.utf8": [{
+      "material": "floor_inst_3_003",
+      "attribRange": [0, 345],
+      "indexRange": [2760, 247],
+      "bboxes": 3501,
+      "names": ["floor_inst_3_003"],
+      "lengths": [741]
+    }],
+    "8b3c3382.model.utf8": [{
+      "material": "floor_inst_4_003",
+      "attribRange": [0, 264],
+      "indexRange": [2112, 156],
+      "bboxes": 2580,
+      "names": ["floor_inst_4_003"],
+      "lengths": [468]
+    }],
+    "b6bcea3b.model.utf8": [{
+      "material": "floor_inst_5_003",
+      "attribRange": [0, 544],
+      "indexRange": [4352, 412],
+      "bboxes": 5588,
+      "names": ["floor_inst_5_003"],
+      "lengths": [1236]
+    }],
+    "26d48c33.model.utf8": [{
+      "material": "ground_level_frames_003",
+      "attribRange": [0, 33804],
+      "indexRange": [270432, 18892],
+      "bboxes": 327108,
+      "names": ["ground_level_frames_003"],
+      "lengths": [56676]
+    }],
+    "63f7d493.model.utf8": [{
+      "material": "inner_wall_1_003",
+      "attribRange": [0, 3977],
+      "indexRange": [31816, 3942],
+      "bboxes": 43642,
+      "names": ["inner_wall_1_003"],
+      "lengths": [11826]
+    }],
+    "84330020.model.utf8": [{
+      "material": "inner_wall_2_003",
+      "attribRange": [0, 4095],
+      "indexRange": [32760, 4430],
+      "bboxes": 46050,
+      "names": ["inner_wall_2_003"],
+      "lengths": [13290]
+    }],
+    "8ee1a7cb.model.utf8": [{
+      "material": "inner_wall_3_003",
+      "attribRange": [0, 4837],
+      "indexRange": [38696, 5152],
+      "bboxes": 54152,
+      "names": ["inner_wall_3_003"],
+      "lengths": [15456]
+    }],
+    "1596d9f9.model.utf8": [{
+      "material": "inner_wall_4_003",
+      "attribRange": [0, 3882],
+      "indexRange": [31056, 3800],
+      "bboxes": 42456,
+      "names": ["inner_wall_4_003"],
+      "lengths": [11400]
+    }],
+    "5edd5c4c.model.utf8": [{
+      "material": "new_roof_003",
+      "attribRange": [0, 126],
+      "indexRange": [1008, 74],
+      "bboxes": 1230,
+      "names": ["new_roof_003"],
+      "lengths": [222]
+    }],
+    "5735200b.model.utf8": [{
+      "material": "side_walk_003",
+      "attribRange": [0, 873],
+      "indexRange": [6984, 850],
+      "bboxes": 9534,
+      "names": ["side_walk_003"],
+      "lengths": [2550]
+    }],
+    "1df120da.model.utf8": [{
+      "material": "standard_glass",
+      "attribRange": [0, 26450],
+      "indexRange": [211600, 14892],
+      "bboxes": 256276,
+      "names": ["exterior_window_glass"],
+      "lengths": [44676]
+    }],
+    "1877894c.model.utf8": [{
+      "material": "street_names",
+      "attribRange": [0, 16],
+      "indexRange": [128, 8],
+      "bboxes": 152,
+      "names": ["street_names"],
+      "lengths": [24]
+    }],
+    "6105a269.model.utf8": [{
+      "material": "walkway_railing_003",
+      "attribRange": [0, 9896],
+      "indexRange": [79168, 7940],
+      "bboxes": 102988,
+      "names": ["walkway_railing_003"],
+      "lengths": [23820]
+    }]
+  },
+  "size": 28070645
+}
---
+++ portfolio/terminal-stores/js/config.js
@@ -0,0 +1,28 @@
+var config = {
+  baseUrl: '',
+  backgroundColor: 'rgb(200, 200, 200)',
+  zoomDistance: {
+    init: 150,
+    min: 150,
+    max: 300
+  },
+  orbitAngle: {
+    min: 0,
+    max: Math.PI / 2
+  },
+  model: {
+    url: './data/ionic/model.js'
+  },
+  progress: {
+    current: 0
+  },
+  cards: {
+    url: '/data/ionic/cards/',
+    json: 'cards.json',
+    key: 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=1YvF9U7nR3VqzaivHjKV8Yh9QrfCfILUnZc9O1A3-er0&output=html'
+  },
+  groups: {
+    url: './data/ionic/cards/',
+    json: 'floors.json'
+  }
+};
